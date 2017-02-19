9092ee71f6be71a90cc5de532694de409e81bf1e
bfgard <bfgard@gmail.com>
2017-02-19 03:17:23 -0700

    Fully functioning GUI and saving capabilities on subscriber

--- vision_ws/src/sniper_cam/scripts/sniper_cam_subscriber.py
+++ vision_ws/src/sniper_cam/scripts/sniper_cam_subscriber.py
@@ -9,13 +9,15 @@
 ## -maybe subscribe to compressed image data??
 
 import rospy
+import os.path
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
 # create CvBridge object
 bridge = CvBridge()
-
+global_var={'image_n':0}
+globals().update(global_var)
 def image_callback(msg):
     try:
         # Convert your ROS Image message to OpenCV2
@@ -27,7 +29,9 @@ def image_callback(msg):
         cv2.imshow('sniper_cam', cv2_img)
         cv2.waitKey(1)
         # Save the image
-        #cv2.imwrite('camera_image.jpeg', cv2_img)
+        cv2.imwrite('images/image' + str(image_n)+ '.jpeg', cv2_img)
+	global image_n
+	image_n +=1
 
 def image_subscriber():
     rospy.init_node('sniper_image_subscriber')
@@ -38,3 +42,6 @@ def image_subscriber():
 
 if __name__ == '__main__':
     image_subscriber()
+    imagedir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images')
+    if not os.path.isdir(imagedir):
+	os.makedires(imagedir)
